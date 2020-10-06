86bed6e001a40ca61c7d4cc6dd1766a9c04f288a
Lohan Luiz <lohan.luiz@emdurportovelho.com>
2020-10-06 10:13:01 -0400

    Finished the CSS of Main page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
 
 export default function Main({ match }) {
   return (
-  <h1>{match.params.id}</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev"/>
+      <ul>
+
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt=""/>
+          <footer>
+            <strong>Robson Marques</strong>
+            <p>Co-founder e CEO @Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt=""/>
+          <footer>
+            <strong>Robson Marques</strong>
+            <p>Co-founder e CEO @Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/861751?v=4" alt=""/>
+          <footer>
+            <strong>Robson Marques</strong>
+            <p>Co-founder e CEO @Rocketseat</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="Like"/>
+            </button>
+          </div>
+        </li>
+
+
+      </ul>
+    </div>
   );
 }
