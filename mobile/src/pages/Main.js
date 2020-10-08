d556528fc96b98f6927b3840684a6b47b3d6ca5a
Lohan Luiz <lohan.luiz@emdurportovelho.com>
2020-10-08 10:20:14 -0400

    Main page styles

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { SafeAreaView, Image, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import logo from '../assets/logo.png';
+import like from '../assets/like.png';
+import dislike from '../assets/dislike.png';
 
 export default function Main() {
   return (
-    <SafeAreaView>
-      <Image source={logo} />
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+        <View style={styles.cardsContainer}>
+
+          <View style={[styles.card, {zIndex: 3 }]}>
+            <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/6643122?v=4'}} />
+            <View style={styles.footer}>
+              <Text style={styles.name}>Mayk Brito</Text>
+              <Text style={styles.bio} numberOfLines={3}>An instructor focused on helping people start programming for web - #html #css #javascript #sql #react #nodejs #fullstack</Text>
+            </View>
+          </View>
+
+        </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Image source={dislike} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Image source={like} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -17,5 +41,53 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+
+  logo: {
+    marginTop: 30,
+  },
+
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+
+  card: {
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+  avatar: {
+    flex: 1,
+    height: 300,
+  },
+
+  footer: {
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  bio: {
+    fontSize: 14,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 18,
   },
 });
